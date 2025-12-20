71d7ee1dd36b4fbf99bb9d468631bfd910049b32
skppatil1997-stack <skppatil1997@gmail.com>
2025-12-20 09:21:13 +0000

    Added database name

--- config/db.js
+++ config/db.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 
-module.exports = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+    console.log("Connected DB name:", conn.connection.name);
+  } catch (err) {
+    console.error("MongoDB connection failed", err);
+    process.exit(1);
+  }
 };
+
+module.exports = connectDB;
