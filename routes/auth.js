e7f9246c6d0958c029941d64057f029315d87e56
skppatil1997-stack <skppatil1997@gmail.com>
2025-12-25 09:07:49 +0000

    fixed Admin Signup after db reset and lowercase admin resolved

--- routes/auth.js
+++ routes/auth.js
@@ -140,7 +140,7 @@ router.post("/login", async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
-        role: user.role.toLowerCase()
+        role: user.role
       }
     });
   } catch (err) {
